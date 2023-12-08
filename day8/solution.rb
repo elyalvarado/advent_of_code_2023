be26b7f94b699fb8d8b470a824b6a13ac485ce76
Ely Alvarado <elyalvarado@gmail.com>
2023-12-08 14:14:26 -0500

    Day 8: part 2 tests working

--- day8/solution.rb
+++ day8/solution.rb
@@ -53,6 +53,37 @@ class NetworkMap
   end
 end
 
+class NetworkMapMultiplePath < NetworkMap
+
+  def initialize(map_doc)
+    instructions_doc, nodes_doc = map_doc.split("\n\n")
+    @nodes = parse_nodes(nodes_doc)
+    @current_instruction = parse_instructions(instructions_doc)
+    @moves = 0
+    @current_nodes = initial_nodes
+  end
+
+  def move!
+    return false if finished?
+    next_nodes = @current_nodes.map { |current_node| nodes[current_node][@current_instruction.index_value] }
+    @current_instruction = @current_instruction.next
+    @moves += 1
+    @current_nodes = next_nodes
+    true
+  end
+
+  private
+  def initial_nodes
+    @nodes.keys.select { |k| k[-1] == 'A' }
+  end
+
+  def finished?
+    @current_nodes.inject(true) { |memo, node| memo && node[-1] == 'Z' }
+  end
+
+
+end
+
 class Instruction
   attr_accessor :value, :next
 
@@ -79,4 +110,8 @@ if __FILE__ == $PROGRAM_NAME
   network_map = NetworkMap.new(doc)
   network_map.complete!
   puts network_map.moves
+
+  multiple_network_map = NetworkMapMultiplePath(doc)
+  multiple_network_map.complete!
+  puts multiple_network_map.moves
 end
--- day8/test.rb
+++ day8/test.rb
@@ -66,4 +66,23 @@ class Day8 < Minitest::Test
     assert_equal 6, network_map.moves
   end
 
+  def test_complete_multiple_paths
+    mult_map = <<~DOC
+      LR
+
+      11A = (11B, XXX)
+      11B = (XXX, 11Z)
+      11Z = (11B, XXX)
+      22A = (22B, XXX)
+      22B = (22C, 22C)
+      22C = (22Z, 22Z)
+      22Z = (22B, 22B)
+      XXX = (XXX, XXX)
+    DOC
+
+    network_map = NetworkMapMultiplePath.new(mult_map)
+    network_map.complete!
+    assert_equal 6, network_map.moves
+  end
+
 end
