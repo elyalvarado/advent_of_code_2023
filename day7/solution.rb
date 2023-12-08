4015bbf4f1cae3aa5098ab4587bc535dbf2c6f3c
Ely Alvarado <elyalvarado@gmail.com>
2023-12-07 23:37:19 -0500

    Day 7: part 1, all comparisons tested

--- day7/solution.rb
+++ day7/solution.rb
@@ -6,15 +6,39 @@ class Hand
   end
 
   def <=>(another)
+    return hex_value <=> another.hex_value if repeat_value == another.repeat_value
     repeat_value <=> another.repeat_value
   end
 
   def repeat_value
-    counts[0] * 10e4 +
+    @repeat_value ||= counts[0] * 10e4 +
       (counts[1] || 0) * 10e3 +
       (counts[2] || 0) * 10e2 +
       (counts[3] || 0) * 10e1 +
       (counts[4] || 0)
+    # puts "repeat value for #{@hand}: #{@repeat_value}"
+    # @repeat_value
+  end
+
+  def hex_value
+    @hex_value ||= cards.map do |card|
+      case card
+      when 'A'
+        'F'
+      when 'K'
+        'E'
+      when 'Q'
+        'D'
+      when 'K'
+        'C'
+      when 'T'
+        'B'
+      else
+        card
+      end
+    end.join.to_i(16)
+    # puts "hex value for #{@hand}: #{@hex_value}"
+    # @hex_value
   end
 
   def counts
--- day7/test.rb
+++ day7/test.rb
@@ -27,4 +27,16 @@ class Day7 < Minitest::Test
     assert_equal true, Hand.new('11234') > Hand.new('12345')
   end
 
+  def test_high_card_no_repeats
+    assert_equal true, Hand.new('54321') > Hand.new('12345')
+  end
+
+  def test_high_card_five
+    assert_equal true, Hand.new('22222') > Hand.new('11111')
+  end
+
+  def test_high_card_four
+    assert_equal true, Hand.new('21111') > Hand.new('1AAAA')
+  end
+
 end
