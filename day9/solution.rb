2ad39f98a10c9eca9c0e40885a2b32e1e394fe80
Ely Alvarado <elyalvarado@gmail.com>
2023-12-09 21:25:45 -0500

    Day 9: part 1 add test for border case that was failing

--- day9/solution.rb
+++ day9/solution.rb
@@ -10,32 +10,39 @@ class History
   end
 
   def next
-    # puts @matrix.inspect
+    debug(@matrix)
     current_line = 1
     current_index = @numbers.length - 2
     while true do
       current_value = calculate_down(current_line, current_index)
       current_line += 1
       current_index -= 1
-      # puts "calculated down value #{current_value}"
-      # puts @matrix.inspect
+      puts "calculated down value #{current_value}"
+      debug(@matrix)
       break if current_value == 0
     end
     n = calculate_up(0, @numbers.length)
-    # puts @matrix.inspect
-    # n
+    debug(@matrix)
+    n
   end
 
   def self.sum_extrapolated(doc)
-    doc.split("\n").map { |line| History.new(line).next }.reduce(:+)
+    next_values = doc.split("\n").map do |line|
+      puts line
+      n = History.new(line).next
+      puts n
+      n
+    end
+    next_values.reduce(:+)
   end
 
   private
   def calculate_down(line, index)
-    # puts "calcualte down Line #{line} index #{index}"
+    puts "calcualte down Line #{line} index #{index}"
     return @matrix[line][index] unless @matrix[line][index].nil?
     @matrix[line][index] = calculate_down(line-1,index+1) - calculate_down(line-1,index)
-    if @matrix[line][index] == 0
+    if @matrix[line][index] == 0 && @matrix[line][index+1].nil?
+      puts "Filling with zeros line #{line}, upto index #{index} because "
       (0...index).each do |i|
         @matrix[line][i] = 0
       end
@@ -44,20 +51,33 @@ class History
   end
 
   def calculate_up(line, index)
-    # puts "calculate up Line #{line} index #{index}"
+    puts "calculate up Line #{line} index #{index}"
     return @matrix[line][index] unless @matrix[line][index].nil?
     if @matrix[line+1][index-2] == 0
       @matrix[line+1][index-1] = 0
       @matrix[line][index] = @matrix[line][index-1]
-      # puts @matrix.inspect
+      debug(@matrix)
       return @matrix[line][index]
     end
     @matrix[line][index] = @matrix[line][index-1] + calculate_up(line+1,index-1)
-    # puts @matrix.inspect
-    # @matrix[line][index]
+    debug(@matrix)
+    @matrix[line][index]
+  end
+
+  def debug(matrix)
+    matrix.each do |l|
+      line = l.dup
+      break if line.compact.length == 0
+      while line.pop.nil?
+        true
+      end
+      line.map! { |v| v.nil? ? 'X' : v }
+      puts line.join('|')
+    end
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
-
+  doc = File.read('input.txt')
+  puts History.sum_extrapolated(doc)
 end
--- day9/test.rb
+++ day9/test.rb
@@ -8,19 +8,23 @@ class Day9 < Minitest::Test
     10 13 16 21 30 45
   DOC
 
-  def test_assert_next_1
+  def xtest_assert_next_1
     assert_equal 18, History.new('0 3 6 9 12 15').next
   end
 
-  def test_assert_next_2
+  def xtest_assert_next_2
     assert_equal 28, History.new('1 3 6 10 15 21').next
   end
 
-  def test_assert_next_3
+  def xtest_assert_next_3
     assert_equal 68, History.new('10 13 16 21 30 45').next
   end
 
-  def test_sum_extrapolated
+  def test_assert_next_4
+    assert_equal 16489171, History.new('-6 -13 -17 3 93 334 845 1783 3355 5876 9945 16889 29783 55659 110077 226237 472643 986772 2038810 4152178 8331240').next
+  end
+
+  def xtest_sum_extrapolated
     assert_equal 114, History.sum_extrapolated(REPORT)
   end
 end
