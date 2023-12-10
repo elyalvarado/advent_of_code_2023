44a0f4df906b2228b29b4bcaa602e2cdc33d96ea
Ely Alvarado <elyalvarado@gmail.com>
2023-12-09 22:40:33 -0500

    Day 9: part 1 solution

--- day9/solution.rb
+++ day9/solution.rb
@@ -9,28 +9,65 @@ class History
     end
   end
 
+  def next2
+    current_line_index = 1
+    # puts 'filling down'
+    while true do
+      current_line = @matrix[current_line_index]
+      previous_line = @matrix[current_line_index - 1]
+      max_index = current_line.length - current_line_index - 2
+      (0..max_index).each do |i|
+        current_line[i] = previous_line[i+1]-previous_line[i]
+      end
+
+      current_line_values = current_line[0..max_index]
+      all_zeroes = current_line_values.uniq == [0]
+      # debug(@matrix)
+      break if all_zeroes
+      current_line_index += 1
+    end
+
+    # puts 'adding another zero'
+    current_line = @matrix[current_line_index]
+    add_index = current_line.length - current_line_index - 1
+    current_line[add_index] = 0
+    current_line_index -= 1
+    # debug(@matrix)
+
+    # puts 'filling up'
+    while true do
+      current_line = @matrix[current_line_index]
+      next_line = @matrix[current_line_index + 1]
+      max_index = current_line.length - current_line_index - 1
+      current_line[max_index] = current_line[max_index-1] + next_line[max_index-1]
+      # debug(@matrix)
+      return current_line[max_index] if current_line_index == 0
+      current_line_index -= 1
+    end
+  end
+
   def next
-    debug(@matrix)
+    # debug(@matrix)
     current_line = 1
     current_index = @numbers.length - 2
     while true do
       current_value = calculate_down(current_line, current_index)
       current_line += 1
       current_index -= 1
-      puts "calculated down value #{current_value}"
-      debug(@matrix)
+      # puts "calculated down value #{current_value}"
+      # debug(@matrix)
       break if current_value == 0
     end
     n = calculate_up(0, @numbers.length)
-    debug(@matrix)
-    n
+    # debug(@matrix)
+    # n
   end
 
   def self.sum_extrapolated(doc)
     next_values = doc.split("\n").map do |line|
-      puts line
-      n = History.new(line).next
-      puts n
+      # puts line.split(' ').join(",")
+      n = History.new(line).next2
+      # puts n
       n
     end
     next_values.reduce(:+)
@@ -38,30 +75,30 @@ class History
 
   private
   def calculate_down(line, index)
-    puts "calcualte down Line #{line} index #{index}"
+    # puts "calcualte down Line #{line} index #{index}"
     return @matrix[line][index] unless @matrix[line][index].nil?
     @matrix[line][index] = calculate_down(line-1,index+1) - calculate_down(line-1,index)
-    if @matrix[line][index] == 0 && @matrix[line][index+1].nil?
-      puts "Filling with zeros line #{line}, upto index #{index} because "
-      (0...index).each do |i|
-        @matrix[line][i] = 0
-      end
-    end
+    # if @matrix[line][index] == 0 && @matrix[line][index+1].nil?
+    #   # puts "Filling with zeros line #{line}, upto index #{index} because "
+    #   (0...index).each do |i|
+    #     @matrix[line][i] = 0
+    #   end
+    # end
     @matrix[line][index]
   end
 
   def calculate_up(line, index)
-    puts "calculate up Line #{line} index #{index}"
+    # puts "calculate up Line #{line} index #{index}"
     return @matrix[line][index] unless @matrix[line][index].nil?
     if @matrix[line+1][index-2] == 0
       @matrix[line+1][index-1] = 0
       @matrix[line][index] = @matrix[line][index-1]
-      debug(@matrix)
+      # debug(@matrix)
       return @matrix[line][index]
     end
     @matrix[line][index] = @matrix[line][index-1] + calculate_up(line+1,index-1)
-    debug(@matrix)
-    @matrix[line][index]
+    # debug(@matrix)
+    # @matrix[line][index]
   end
 
   def debug(matrix)
@@ -74,6 +111,7 @@ class History
       line.map! { |v| v.nil? ? 'X' : v }
       puts line.join('|')
     end
+    puts '--'
   end
 end
 
--- day9/test.rb
+++ day9/test.rb
@@ -8,23 +8,23 @@ class Day9 < Minitest::Test
     10 13 16 21 30 45
   DOC
 
-  def xtest_assert_next_1
-    assert_equal 18, History.new('0 3 6 9 12 15').next
+  def test_assert_next_1
+    assert_equal 18, History.new('0 3 6 9 12 15').next2
   end
 
-  def xtest_assert_next_2
-    assert_equal 28, History.new('1 3 6 10 15 21').next
+  def test_assert_next_2
+    assert_equal 28, History.new('1 3 6 10 15 21').next2
   end
 
-  def xtest_assert_next_3
-    assert_equal 68, History.new('10 13 16 21 30 45').next
+  def test_assert_next_3
+    assert_equal 68, History.new('10 13 16 21 30 45').next2
   end
 
   def test_assert_next_4
-    assert_equal 16489171, History.new('-6 -13 -17 3 93 334 845 1783 3355 5876 9945 16889 29783 55659 110077 226237 472643 986772 2038810 4152178 8331240').next
+    assert_equal 16489171, History.new('-6 -13 -17 3 93 334 845 1783 3355 5876 9945 16889 29783 55659 110077 226237 472643 986772 2038810 4152178 8331240').next2
   end
 
-  def xtest_sum_extrapolated
+  def test_sum_extrapolated
     assert_equal 114, History.sum_extrapolated(REPORT)
   end
 end
