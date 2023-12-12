0c5341868cd05e05ece879647701e7e9ad91b5ac
Ely Alvarado <elyalvarado@gmail.com>
2023-12-12 09:46:08 -0500

    Day 12: part 1 tests working

--- day12/solution.rb
+++ day12/solution.rb
@@ -1,33 +1,49 @@
 class SpringsMap
   attr_reader :springs
   def initialize(doc)
+    @springs = doc.split("\n").map { |line| Spring.new(line)}
+  end
 
+  def arrangements
+    @springs.inject(0) { |sum, spring| sum += spring.arrangements; sum }
   end
 end
 
 class Spring
   def initialize(doc)
     @og_string, groups = doc.split(' ')
-    @groups = groups.split(',').map(&:to_i).sort
+    @groups = groups.split(',').map(&:to_i)
   end
 
   def arrangements
-
+    alternatives.inject(0) do |count, alternative|
+      # puts "Evaluating alternative #{alternative} for #{@groups.inspect}"
+      alternative_groups = (alternative.split('.')-['']).map(&:size)
+      # puts "Alternative groups: #{alternative_groups}"
+      # puts "Valid alternative" if alternative_groups == @groups
+      count += 1 if alternative_groups == @groups
+      # puts '---'
+      count
+    end
   end
 
   private
   def alternatives
     @alternatives ||= alternatives_for(@og_string)
+    # puts @alternatives.inspect
+    # puts "Total #{@alternatives.size} #{@alternatives.uniq.size} uniq"
+    @alternatives
   end
 
   def alternatives_for(string)
-    return [ string ] unless string.include('?')
+    return [ string ] unless string.include?('?')
     first_question_mark = string.index('?')
     with_dot = string.dup
     with_dot[first_question_mark] = '.'
-    with_question_mark = string.dup
-    with_question_mark[first_question_mark] = '?'
-    alternatives_for(with_dot) + alternatives_for(with_question_mark)
+    with_hash = string.dup
+    with_hash[first_question_mark] = '#'
+    # puts "#{string}: #{with_dot} #{with_hash}"
+    alternatives_for(with_dot) + alternatives_for(with_hash)
   end
 end
 
--- day12/test.rb
+++ day12/test.rb
@@ -15,4 +15,27 @@ class Day12 < Minitest::Test
     assert_equal 1, Spring.new('???.### 1,1,3').arrangements
   end
 
+  def test_spring_arrangements_2
+    assert_equal 4, Spring.new('.??..??...?##. 1,1,3').arrangements
+  end
+
+  def test_spring_arrangement_3
+    assert_equal 1, Spring.new('?#?#?#?#?#?#?#? 1,3,1,6').arrangements
+  end
+
+  def test_spring_arrangement_4
+    assert_equal 1, Spring.new('????.#...#... 4,1,1').arrangements
+  end
+
+  def test_spring_arrangement_5
+    assert_equal 4, Spring.new('????.######..#####. 1,6,5').arrangements
+  end
+
+  def test_spring_arrangement_6
+    assert_equal 10, Spring.new('?###???????? 3,2,1').arrangements
+  end
+
+  def test_spring_map_arrangements
+    assert_equal 21, SpringsMap.new(SPRINGSMAP).arrangements
+  end
 end
