73c5cd83181f98c0b7efb6065bcc76937e17a8e7
Ely Alvarado <elyalvarado@gmail.com>
2023-12-13 09:08:39 -0500

    Day 13: part 1 tests working

--- day13/solution.rb
+++ day13/solution.rb
@@ -1,4 +1,62 @@
+class Mirror
+  attr_reader :width, :height, :matrix
+  def initialize(doc)
+    @matrix = doc.split("\n").map { |line| line.split("") }
+    @width = @matrix[0].size
+    @height = @matrix.size
+  end
 
+  def vertical_lor
+    # lors = []
+    (1..(width-2)).each do |row_index|
+      # puts "Evaluating row_index #{row_index}"
+      is_lor = true
+      max_mirror_width = [row_index, width - row_index].min
+      # puts "- max_mirror_width #{max_mirror_width}"
+      max_mirror_width.times do |delta|
+        # puts "  - delta #{delta}"
+        height.times do |line_index|
+          left_char = matrix[line_index][row_index - delta - 1]
+          right_char = matrix[line_index][row_index + delta]
+          match =  left_char == right_char
+          # puts "Line #{line_index}: Left #{left_char}, Right #{right_char}, Match #{match.inspect}"
+          is_lor = is_lor && match
+          break unless is_lor
+        end
+        break unless is_lor
+      end
+      # puts "- row_index #{row_index} is_lor: #{is_lor.inspect}"
+      return row_index if is_lor
+    end
+    # lors
+    0
+  end
+
+  def horizontal_lor
+    # lors = []
+    (1..(height-2)).each do |line_index|
+      # puts "Evaluating line_index #{line_index}"
+      is_lor = true
+      max_mirror_height = [line_index, height - line_index].min
+      # puts "- max_mirror_height #{max_mirror_height}"
+      max_mirror_height.times do |delta|
+        # puts "  - delta #{delta}"
+        width.times do |row_index|
+          above_char = matrix[line_index - delta - 1][row_index]
+          below_char = matrix[line_index + delta][row_index]
+          match = above_char == below_char
+          # puts "Row #{row_index}: Above #{above_char}, Below #{below_char}, Match #{match.inspect}"
+          is_lor = is_lor && match
+          break unless is_lor
+        end
+      end
+      # puts "- line_index #{line_index} is_lor: #{is_lor.inspect}"
+      return line_index if is_lor
+    end
+    # lors
+    0
+  end
+end
 
 if __FILE__ == $PROGRAM_NAME
   doc = File.read('input.txt')
--- day13/test.rb
+++ day13/test.rb
@@ -2,5 +2,39 @@ require 'minitest/autorun'
 require_relative 'solution'  # Replace with the actual path
 
 class Day < Minitest::Test
+  MIRRORDOC1 = <<~DOC
+    #.##..##.
+    ..#.##.#.
+    ##......#
+    ##......#
+    ..#.##.#.
+    ..##..##.
+    #.#.##.#.
+  DOC
+
+  MIRRORDOC2 = <<~DOC
+    #...##..#
+    #....#..#
+    ..##..###
+    #####.##.
+    #####.##.
+    ..##..###
+    #....#..#
+  DOC
+
+  def test_vertical_lor
+    assert_equal 5, Mirror.new(MIRRORDOC1).vertical_lor
+  end
+
+  def test_vertical_lor_zero
+    assert_equal 0, Mirror.new(MIRRORDOC2).vertical_lor
+  end
+  def test_horizontal_lor
+    assert_equal 4, Mirror.new(MIRRORDOC2).horizontal_lor
+  end
+
+  def test_horizontal_lor_zero
+    assert_equal 0, Mirror.new(MIRRORDOC1).horizontal_lor
+  end
 
 end
