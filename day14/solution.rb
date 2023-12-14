d7d59d628ad81feccbddfa7f3e313509121702eb
Ely Alvarado <elyalvarado@gmail.com>
2023-12-14 07:48:37 -0500

    Day 14: part 1 tests passing

--- day14/solution.rb
+++ day14/solution.rb
@@ -1,4 +1,45 @@
 
+class Parabolic
+  attr_reader :matrix, :width, :height
+  def initialize(doc)
+    @matrix = doc.split("\n").map { |line| line.split("") }
+    @width = @matrix.first.size
+    @height = @matrix.size
+  end
+
+  def total_load
+    total_load = 0
+    width.times do |row|
+      last_round_rock = -1
+      last_cube_rock = -1
+      height.times do |line|
+        puts "line #{line} row #{row}: char #{matrix[line][row]} (last_cube: #{last_cube_rock}, last_round: #{last_round_rock}"
+        case matrix[line][row]
+        when '#'
+          puts "- Updating last_cube_rock to #{line}"
+          last_cube_rock = line
+        when 'O'
+          if last_cube_rock > last_round_rock
+            move_to_line = last_cube_rock + 1
+            last_round_rock = move_to_line
+            total_load += height - move_to_line
+            puts "- Moving round rock and setting last_round to #{move_to_line}"
+            puts "- Incrementing score to: #{total_load} (increment #{height - move_to_line})"
+          else
+            move_to_line = last_round_rock + 1
+            last_round_rock = move_to_line
+            total_load += height - move_to_line
+            puts "- Moving round rock and setting last_round to #{move_to_line}"
+            puts "- Incrementing score to: #{total_load} (increment #{height - move_to_line})"
+          end
+        else
+          puts '- Nothing to do here'
+        end
+      end
+    end
+    total_load
+  end
+end
 
 if __FILE__ == $PROGRAM_NAME
   doc = File.read('input.txt')
--- day14/test.rb
+++ day14/test.rb
@@ -2,5 +2,25 @@ require 'minitest/autorun'
 require_relative 'solution'  # Replace with the actual path
 
 class Day < Minitest::Test
+  DOC = <<~DOC
+    O....#....
+    O.OO#....#
+    .....##...
+    OO.#O....O
+    .O.....O#.
+    O.#..O.#.#
+    ..O..#O..O
+    .......O..
+    #....###..
+    #OO..#....
+  DOC
 
+  def test_read_matrix
+    test_doc = "0..\n.0.\n..0"
+    assert_equal [ %w{0 . .}, %w{. 0 .}, %w{. . 0}], Parabolic.new(test_doc).matrix
+  end
+
+  def test_total_load
+    assert_equal 136, Parabolic.new(DOC).total_load
+  end
 end
