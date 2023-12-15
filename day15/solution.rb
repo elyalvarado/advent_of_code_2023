79ae92dcbafd9b1b5143a70b430858549cb16e67
Ely Alvarado <elyalvarado@gmail.com>
2023-12-15 15:05:04 -0500

    day 15: part1 tests working

--- day15/solution.rb
+++ day15/solution.rb
@@ -1,4 +1,30 @@
 
+class SequenceHasher
+  def initialize(sequence)
+    @sequence = sequence
+    @instructions = @sequence.split(',')
+  end
+
+  def hash
+    @instructions.inject(0) { |sum, instruction| sum += Hasher.new(instruction).hash; sum }
+  end
+end
+
+class Hasher
+  def initialize(string)
+    @string = string
+  end
+
+  def hash
+    current_value = 0
+    @string.split('').each do |char|
+      current_value += char.ord
+      current_value *= 17
+      current_value = current_value % 256
+    end
+    current_value
+  end
+end
 
 if __FILE__ == $PROGRAM_NAME
   doc = File.read('input.txt')
--- day15/test.rb
+++ day15/test.rb
@@ -3,4 +3,11 @@ require_relative 'solution'  # Replace with the actual path
 
 class Day < Minitest::Test
 
+  def test_hash_1
+    assert_equal 52, Hasher.new('HASH').hash
+  end
+
+  def test_sequence_hasher
+    assert_equal 1320, SequenceHasher.new('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7').hash
+  end
 end
