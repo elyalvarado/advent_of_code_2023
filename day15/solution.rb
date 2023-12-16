2b4f54b8171f7963880727cf223e6a5722a803a4
Ely Alvarado <elyalvarado@gmail.com>
2023-12-15 22:42:22 -0500

    Day 15: part 2 tests working

--- day15/solution.rb
+++ day15/solution.rb
@@ -26,6 +26,99 @@ class Hasher
   end
 end
 
+class Lens
+  include Comparable
+
+  attr_accessor :label, :focal_length
+  def initialize(label, focal_length)
+    @label = label
+    @focal_length = focal_length
+  end
+
+  def ==(another)
+    self.label == another.label && self.focal_length == another.focal_length
+  end
+end
+
+class Processor
+  def initialize(doc)
+    @registers = Registers.new
+    @hashes = {}
+    process(doc)
+  end
+
+  def registers
+    @registers.registers
+  end
+
+  def focusing_power
+    @registers.focusing_power
+  end
+
+  private
+  def process(doc)
+    @instructions = doc.split(',')
+    @instructions.each { |instruction| process_instruction(instruction) }
+  end
+
+  def process_instruction(instruction)
+    case instruction
+    when /=/
+      equal(instruction)
+    when /-/
+      minus(instruction)
+    end
+  end
+
+  def equal(instruction)
+    label, focal_length = instruction.split('=')
+    lens = Lens.new(label, focal_length.to_i)
+    register = hashes(label)
+    @registers.equal(register: register, lens: lens)
+  end
+
+  def minus(instruction)
+    label, focal_length = instruction.split('-')
+    lens = Lens.new(label, focal_length)
+    register = hashes(label)
+    @registers.minus(register: register, lens: lens)
+  end
+
+  def hashes(label)
+    return @hashes[label] if @hashes[label]
+    @hashes[label] = Hasher.new(label).hash
+  end
+end
+
+class Registers
+  attr_reader :registers
+  def initialize
+    @registers = Hash.new { |hash, key| hash[key] = [] }
+  end
+
+  def minus(register:, lens:)
+    registers[register] = registers[register].reject { |registered_lens| registered_lens.label == lens.label }
+  end
+
+  def equal(register:, lens:)
+    registered_lens = registers[register].find { |registered_lens| registered_lens.label == lens.label }
+    registered_lens ? registered_lens.focal_length = lens.focal_length : registers[register] << lens
+  end
+
+  def focusing_power
+    power = 0
+    @registers.each do |box, slots|
+      slots.each_with_index do |lens, index|
+        power += (box + 1)*(index + 1)*(lens.focal_length)
+      end
+    end
+    power
+  end
+end
+
+
 if __FILE__ == $PROGRAM_NAME
-  doc = File.read('input.txt')
+  doc = File.read('input.txt').strip
+  puts doc
+  puts SequenceHasher.new(doc).hash
 end
--- day15/test.rb
+++ day15/test.rb
@@ -10,4 +10,70 @@ class Day < Minitest::Test
   def test_sequence_hasher
     assert_equal 1320, SequenceHasher.new('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7').hash
   end
+
+  def test_registers_equal
+    lens = Lens.new('rn',1)
+    registers = Registers.new
+    registers.equal(register: 0, lens: lens)
+    expected_registers = {
+      0 => [lens]
+    }
+    assert_equal expected_registers, registers.registers
+  end
+
+  def test_registers_minus
+    lens1 = Lens.new('rn',1)
+    lens2 = Lens.new('rn',9)
+    registers = Registers.new
+    registers.equal(register: 0, lens: lens1)
+    registers.minus(register: 0, lens: lens2)
+    expected_registers = {
+      0 => []
+    }
+    assert_equal expected_registers, registers.registers
+  end
+
+  def test_processor_1
+    lens = Lens.new('rn',1)
+    expected_registers = {
+      0 => [lens]
+    }
+    assert_equal expected_registers, Processor.new('rn=1').registers
+  end
+
+  def test_processor_2
+    lens1 = Lens.new('rn',1)
+    expected_registers = {
+      0 => [lens1]
+    }
+    assert_equal expected_registers, Processor.new('rn=1,cm-').registers
+  end
+
+  def test_processor_3
+    lens1 = Lens.new('rn',1)
+    lens2 = Lens.new('qp',3)
+    expected_registers = {
+      0 => [lens1],
+      1 => [lens2]
+    }
+    assert_equal expected_registers, Processor.new('rn=1,cm-,qp=3').registers
+  end
+
+  def test_processor_7
+    lens1 = Lens.new('rn',1)
+    lens2 = Lens.new('cm',2)
+    lens3 = Lens.new('ot',7)
+    lens4 = Lens.new('ab',5)
+    lens5 = Lens.new('pc',6)
+    expected_registers = {
+      0 => [lens1, lens2],
+      1 => [],
+      3 => [lens3, lens4, lens5]
+    }
+    assert_equal expected_registers, Processor.new('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7').registers
+  end
+
+  def test_focusing_power
+    assert_equal 145, Processor.new('rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7').focusing_power
+  end
 end
