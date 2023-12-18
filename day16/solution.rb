0260cfed1086ee25d44e3840b0abf525980a0a8b
Ely Alvarado <elyalvarado@gmail.com>
2023-12-17 22:48:31 -0500

    Day 16: part 2 solution

--- day16/solution.rb
+++ day16/solution.rb
@@ -6,7 +6,7 @@ class Contraption
   end
 
   def energized(initial_position: [0,0], entering_direction: [0,1])
-    directions = DIRECTION_MAPS[@matrix.first.first][entering_direction]
+    directions = DIRECTION_MAPS[@matrix[initial_position[0]][initial_position[1]]][entering_direction]
     stack = directions.map { |direction| [initial_position, direction] }
     energized_hash = Hash.new { |hash, key| hash[key] = [] }
 
@@ -57,32 +57,32 @@ class Contraption
     max = 0
     @height.times do |line|
       energized_left_row = energized(initial_position: [line,0],entering_direction: [0,1])
-      puts "Initial Position: #{[line,0].inspect}, Entering Direction: #{[0,1].inspect}, Energized: #{energized_left_row}"
+      # puts "Initial Position: #{[line,0].inspect}, Entering Direction: #{[0,1].inspect}, Energized: #{energized_left_row}"
       if energized_left_row > max
-        puts "New max: #{energized_left_row}"
+        # puts "New max: #{energized_left_row}"
         max = energized_left_row
       end
 
       energized_right_row = energized(initial_position: [line, @width - 1], entering_direction: [0,-1])
-      puts "Initial Position: #{[line,@width - 1].inspect}, Entering Direction: #{[0,-1].inspect}, Energized: #{energized_right_row}"
+      # puts "Initial Position: #{[line,@width - 1].inspect}, Entering Direction: #{[0,-1].inspect}, Energized: #{energized_right_row}"
       if energized_right_row > max
-        puts "New max: #{energized_right_row}"
+        # puts "New max: #{energized_right_row}"
         max = energized_right_row
       end
     end
 
     @width.times do |row|
       energized_top_line = energized(initial_position: [0,row],entering_direction: [1,0])
-      puts "Initial Position: #{[0,row].inspect}, Entering Direction: #{[1,0].inspect}, Energized: #{energized_top_line}"
+      # puts "Initial Position: #{[0,row].inspect}, Entering Direction: #{[1,0].inspect}, Energized: #{energized_top_line}"
       if energized_top_line > max
-        puts "New max: #{energized_top_line}"
+        # puts "New max: #{energized_top_line}"
         max = energized_top_line
       end
 
       energized_bottom_line = energized(initial_position: [@height - 1,row], entering_direction: [-1,0])
-      puts "Initial Position: #{[@height-1,row].inspect}, Entering Direction: #{[-1,0].inspect}, Energized: #{energized_bottom_line}"
+      # puts "Initial Position: #{[@height-1,row].inspect}, Entering Direction: #{[-1,0].inspect}, Energized: #{energized_bottom_line}"
       if energized_bottom_line > max
-        puts "New max: #{energized_bottom_line}"
+        # puts "New max: #{energized_bottom_line}"
         max = energized_bottom_line
       end
     end
@@ -133,4 +133,5 @@ end
 if __FILE__ == $PROGRAM_NAME
   doc = File.read('input.txt')
   puts Contraption.new(doc).energized
+  puts Contraption.new(doc).max_energized
 end
